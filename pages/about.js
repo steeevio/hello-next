aed11f8ed9b4865bfbdeefb7f925bc294e743856
steeevio <stejy@hotmail.com>
2019-05-27 17:05:01 +0100

    another commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Header from '../components/Header';
+import Layout from '../components/myLayout';
+
+export default function About() {
+  return (
+    <div>
+    	<Layout>
+    		<p>This is the about page</p>
+    	</Layout>
+    </div>
+  )
+}
